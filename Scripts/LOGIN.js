d13f31916baedb788999f82f46d4b0327d6f737e
Handson-A <hayelgum@st.ug.edu.gh>
2025-01-09 22:27:06 +0000

    Link to dash from Login

--- Scripts/LOGIN.js
+++ Scripts/LOGIN.js
@@ -1,14 +1,19 @@
 // script.js
-document.getElementById('loginButton').addEventListener('click', function () {
+document.addEventListener('DOMContentLoaded', function () {
+    const loginButton = document.querySelector('button[type="submit"]');
+
+document.getElementById('loginButton').addEventListener('click', function (event) {
+    event.preventDefault();
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
     // Simple validation
     if (username === 'admin' && password === 'admin123') {  
-        window.location.href = 'dashboard.html';
+        window.location.href = '../Pages/Dashboard.html';
     } else {
         alert('Invalid username or password');
     }
     // Here you can add further logic, like sending data to a server
 
+});
 });
